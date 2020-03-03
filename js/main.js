eda1bf6c53b2f30908e84afbf54e0af9acc540cf
Lila Bajraktari <bajr0005@algonquinlive.com>
2020-03-03 10:47:04 -0500

    Add to main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-var letterPrompt = prompt('Enter a letter')
+var letterPrompt = prompt('Enter a letter');
+
+var counter;
+
+for (counter = 0; counter < 10; counter++) {
+  document.write(letterPrompt);
+  document.write('<br>');
+}
